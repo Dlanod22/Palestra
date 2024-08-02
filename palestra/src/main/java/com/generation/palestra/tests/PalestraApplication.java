c3a300a15533abd99c27dcefb73ec84ac7abd20d
Donald <gugadonald10@gmail.com>
2024-08-02 17:15:07 +0200

    AggiuntaTests

--- palestra/src/main/java/com/generation/palestra/tests/PalestraApplication.java
+++ palestra/src/main/java/com/generation/palestra/tests/PalestraApplication.java
@@ -1,4 +1,4 @@
-package com.generation.palestra;
+package com.generation.palestra.tests;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
